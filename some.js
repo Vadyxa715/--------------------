1938ba73921141a6b3f9ac376c1164532370882e
Vadyxa715 <vadim_kurdin@mail.ru>
2023-07-21 20:36:00 +0300

    add prodact page

---
+++ some.js
@@ -0,0 +1,7 @@
+$(function(){
+    $(".skill_per").each(function(){
+        $this = $(this);
+            var per = $(this).attr("per");
+                $this.css("width", per + "%");
+                    $this.find(".value").text(per + "%").css("opacity", "1");                            
+    });
